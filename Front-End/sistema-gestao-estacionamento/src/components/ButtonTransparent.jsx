18868b2ccaf09da00b0ba88970884f92b5d10235
Douglas Kuerten <100738618+DouglasKuerten@users.noreply.github.com>
2023-06-04 22:42:04 -0300

    Delete Front-End/sistema-gestao-estacionamento directory

--- Front-End/sistema-gestao-estacionamento/src/components/ButtonTransparent.jsx
+++
@@ -1,9 +0,0 @@
-export default function ButtonTransparent(props) {
-    return (
-        <button
-            type={props.type}
-            {...props}
-            className="text-sm font-semibold leading-6 text-gray-900">
-            {props.children}
-        </button>)
-}
