f9cc11281a5d89b4f63f48ebbbad43b577c9b95f
Bruno Dimon <bruno_gxis@outlook.com>
2023-06-19 21:15:50 -0300

    - Criado o submenu para a aba "Clientes" com as opções de Cadastrar Cliente e Listagem de Clientes - Criado tela para cadastro de cliente (RegisterClient.jsx) - Criado tela para listagem de cliente com o botão para deletar (ViewClients.jsx) - Ajustado a tela de relatórios (Reports.jsx) para que tenha os campos de "Data Entrada" e "Data Saída" como filtros do relatório e valide se estão preenchidos. - Ajustado a tela de relatórios (Reports.jsx) para que crie os botões de gerar o relatório dinamicamente conforme forem sendo adicionados na lista de relatórios existentes. - Criado o relatório de Ranking de Veículos Estacionados. - Ajustado a tela de Registro de Entrada de Veículo para que valide se os campos "Placa" e "Data/Hora Entrada" estão informados

--- front-end/src/screens/EntryVehicle.jsx
+++ front-end/src/screens/EntryVehicle.jsx
@@ -9,7 +9,25 @@ import Alert from '../components/Alert';
 
 export default function EntryVehicle() {
     const [alertProperties, setAlertProperties] = useState({ visible: false, type: '', title: '', text: '' });
+    const [inputValues, setInputValues] = useState({ placa: null, modelo: null, cor: null, dataHoraEntrada: null, cliente: null });
+
+    function hiddenAlert() {
+        setTimeout(() => {
+            setAlertProperties({ visible: false, type: '', title: '', text: '' });
+        }, 8000);
+    }
+
     function registerEntry() {
+        if (inputValues.placa == null) {
+            setAlertProperties({ visible: true, type: 'error', title: 'Erro', text: 'A placa deve ser informada para registrar a entrada do veículo' });
+            hiddenAlert();
+            return
+        } else if (inputValues.dataHoraEntrada == null) {
+            setAlertProperties({ visible: true, type: 'error', title: 'Erro', text: 'A Data/Hora Entrada deve ser informada para registrar a entrada do veículo' });
+            hiddenAlert();
+            return
+        }
+
         let cadastrou = false; //Variavel criada apenas para testar 
         if (cadastrou) {
             setAlertProperties({ visible: true, type: 'sucess', title: 'Sucesso', text: 'O veículo de placa xxxxxx foi cadastrado a entrada com sucesso!' });
@@ -17,9 +35,7 @@ export default function EntryVehicle() {
             setAlertProperties({ visible: true, type: 'error', title: 'Erro', text: 'Erro ao cadastrar entrada do veículo' });
         }
 
-        setTimeout(() => {
-            setAlertProperties({ visible: false, type: '', title: '', text: '' });
-        }, 8000);
+        hiddenAlert();
     }
     return (
         <div className="duration-300">
@@ -31,11 +47,11 @@ export default function EntryVehicle() {
                 <div className="space-y-12">
                     <div className="border-b border-gray-900/10 pb-12">
                         <div className=" grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                            <Input type="text" name="cliente" id="cliente" autoComplete="off" label="Placa" placeholder="" required={true} />
-                            <Input type="text" name="cliente" id="cliente" autoComplete="off" label="Modelo" placeholder="" />
-                            <Input type="text" name="cliente" id="cliente" autoComplete="off" label="Cor" placeholder="" />
-                            <Input type="datetime-local" name="cliente" id="cliente" autoComplete="off" label="Data/Hora Entrada" placeholder="" required={true} />
-                            <Input type="text" name="cliente" id="cliente" autoComplete="off" label="Cliente" placeholder="" />
+                            <Input type="text" name="placa" id="placa" autoComplete="off" label="Placa" placeholder="" required={true} onChange={(value) => setInputValues({ ...inputValues, placa: value.target.value })} />
+                            <Input type="text" name="modelo" id="modelo" autoComplete="off" label="Modelo" placeholder="" onChange={(value) => setInputValues({ ...inputValues, modelo: value.target.value })} />
+                            <Input type="text" name="cor" id="cor" autoComplete="off" label="Cor" placeholder="" onChange={(value) => setInputValues({ ...inputValues, cor: value.target.value })} />
+                            <Input type="datetime-local" name="dataHoraEntrada" id="dataHoraEntrada" autoComplete="off" label="Data/Hora Entrada" placeholder="" required={true} onChange={(value) => setInputValues({ ...inputValues, dataHoraEntrada: value.target.value })} />
+                            <Input type="text" name="cliente" id="cliente" autoComplete="off" label="Cliente" placeholder="" onChange={(value) => setInputValues({ ...inputValues, cliente: value.target.value })} />
 
                         </div>
                     </div>
