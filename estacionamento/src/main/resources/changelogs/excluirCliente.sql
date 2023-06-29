65b63e668449011d39a7fecd79bbb0276af2c344
Vitor Henrique Pereira <129230417+vitorhenriquepereira@users.noreply.github.com>
2023-06-28 22:40:05 -0300

    Update excluirCliente.sql

--- estacionamento/src/main/resources/changelogs/excluirCliente.sql
+++ estacionamento/src/main/resources/changelogs/excluirCliente.sql
@@ -1,47 +1,23 @@
-CREATE OR REPLACE PROCEDURE excluir_cliente (
+CREATE OR REPLACE
+PROCEDURE excluir_cliente (
   p_id_cliente IN NUMBER
 ) AS
-  v_error_message VARCHAR2(200);
-
+  v_error_message VARCHAR2(4000);
 BEGIN
-  
-  -- Excluir registros relacionados na tabela PAGAMENTO
-  DELETE FROM PAGAMENTO
-  WHERE ID_RESERVA IN (SELECT ID_RESERVA FROM RESERVA
-                        WHERE ID_VEICULO IN (SELECT ID_VEICULO
-                                               FROM VEICULO
-                                              WHERE ID_CLIENTE = p_id_cliente
-                                            )
-  );
-  
-  DELETE FROM PAGAMENTO
-  WHERE ID_SOCIO IN (
-    SELECT ID_SOCIO FROM SOCIO WHERE ID_CLIENTE = p_id_cliente
-  );
-  
-  -- Excluir registros relacionados na tabela RESERVA
-  DELETE FROM RESERVA
-  WHERE ID_VEICULO IN (
-    SELECT ID_VEICULO FROM VEICULO WHERE ID_CLIENTE = p_id_cliente
-  );
+  BEGIN
+    DELETE FROM CLIENTE WHERE ID_CLIENTE = p_id_cliente;
+  EXCEPTION
+    WHEN OTHERS THEN
+      IF SQLCODE = -2292 THEN
+        v_error_message := 'Registro filho localizado.';
+      ELSE
+        v_error_message := SQLERRM;
+      END IF;
+  END;
 
-  -- Excluir registros relacionados na tabela SOCIO
-  DELETE FROM SOCIO WHERE ID_CLIENTE = p_id_cliente;
-
-  -- Excluir registros relacionados na tabela VEICULO
-  DELETE FROM VEICULO WHERE ID_CLIENTE = p_id_cliente;
-
-  -- Excluir o cliente
-  DELETE FROM CLIENTE WHERE ID_CLIENTE = p_id_cliente;
-
-  -- Exibir mensagem de sucesso
-  DBMS_OUTPUT.PUT_LINE('Cliente excluído com sucesso.');
-  COMMIT;
-
-EXCEPTION
-  -- Exibir mensagem de erro
-  WHEN OTHERS THEN
-    v_error_message := SQLERRM;
+  IF v_error_message IS NOT NULL THEN
     DBMS_OUTPUT.PUT_LINE('Erro ao excluir o cliente: ' || v_error_message);
-    ROLLBACK;
+  ELSE
+    DBMS_OUTPUT.PUT_LINE('Cliente excluído com sucesso.');
+  END IF;
 END;
